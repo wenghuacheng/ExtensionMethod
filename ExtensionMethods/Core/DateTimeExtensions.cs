f3b10a5f8ea2c16d70cd357ee27dca1b5d96703e
wenghuacheng <593103283@qq.com>
2016-04-11 14:17:18 +0800

    1

---
+++ ExtensionMethods/Core/DateTimeExtensions.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods.Core
+{
+     public static class DateTimeExtensions
+    { 
+        /// <summary>
+        ///     A DateTime extension method that ages the given this.1
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>An int.</returns>
+        public static int Age(this DateTime @this)
+        {
+            if (DateTime.Today.Month < @this.Month ||
+                DateTime.Today.Month == @this.Month &&
+                DateTime.Today.Day < @this.Day)
+            {
+                return DateTime.Today.Year - @this.Year - 1;
+            }
+            return DateTime.Today.Year - @this.Year;
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that elapsed the given datetime.
+        /// </summary>
+        /// <param name="datetime">The datetime to act on.</param>
+        /// <returns>A TimeSpan.</returns>
+        public static TimeSpan Elapsed(this DateTime datetime)
+        {
+            return DateTime.Now - datetime;
+        }
+
+        #region Get Time
+        /// <summary>
+        ///     A DateTime extension method that return a DateTime with the time set to "23:59:59:999". The last moment of
+        ///     the day. Use "DateTime2" column type in sql to keep the precision.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>A DateTime of the day with the time set to "23:59:59:999".</returns>
+        public static DateTime EndOfDay(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, @this.Day).AddDays(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that return a DateTime of the last day of the month with the time set to
+        ///     "23:59:59:999". The last moment of the last day of the month.  Use "DateTime2" column type in sql to keep 
+the
+        ///     precision.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>A DateTime of the last day of the month with the time set to "23:59:59:999".</returns>
+        public static DateTime EndOfMonth(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, 1).AddMonths(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+
+        /// <summary>
+        ///     A System.DateTime extension method that ends of week.
+        /// </summary>
+        /// <param name="dt">Date/Time of the dt.</param>
+        /// <param name="startDayOfWeek">(Optional) the start day of week.</param>
+        /// <returns>A DateTime.</returns>
+        public static DateTime EndOfWeek(this DateTime dt, DayOfWeek startDayOfWeek = DayOfWeek.Sunday)
+        {
+            DateTime end = dt;
+            DayOfWeek endDayOfWeek = startDayOfWeek - 1;
+            if (endDayOfWeek < 0)
+            {
+                endDayOfWeek = DayOfWeek.Saturday;
+            }
+
+            if (end.DayOfWeek != endDayOfWeek)
+            {
+                if (endDayOfWeek < end.DayOfWeek)
+                {
+                    end = end.AddDays(7 - (end.DayOfWeek - endDayOfWeek));
+                }
+                else
+                {
+                    end = end.AddDays(endDayOfWeek - end.DayOfWeek);
+                }
+            }
+
+            return new DateTime(end.Year, end.Month, end.Day, 23, 59, 59, 999);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that return a DateTime of the last day of the year with the time set to
+        ///     "23:59:59:999". The last moment of the last day of the year.  Use "DateTime2" column type in sql to keep 
+the
+        ///     precision.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>A DateTime of the last day of the year with the time set to "23:59:59:999".</returns>
+        public static DateTime EndOfYear(this DateTime @this)
+        {
+            return new DateTime(@this.Year, 1, 1).AddYears(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that first day of week.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>A DateTime.</returns>
+        public static DateTime FirstDayOfWeek(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, @this.Day).AddDays(-(int)@this.DayOfWeek);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that last day of week.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>A DateTime.</returns>
+        public static DateTime LastDayOfWeek(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, @this.Day).AddDays(6 - (int)@this.DayOfWeek);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that return a DateTime with the time set to "00:00:00:000". The first moment 
+of
+        ///     the day.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>A DateTime of the day with the time set to "00:00:00:000".</returns>
+        public static DateTime StartOfDay(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, @this.Day);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that return a DateTime of the first day of the month with the time set to
+        ///     "00:00:00:000". The first moment of the first day of the month.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>A DateTime of the first day of the month with the time set to "00:00:00:000".</returns>
+        public static DateTime StartOfMonth(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, 1);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that starts of week.
+        /// </summary>
+        /// <param name="dt">The dt to act on.</param>
+        /// <param name="startDayOfWeek">(Optional) the start day of week.</param>
+        /// <returns>A DateTime.</returns>
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startDayOfWeek = DayOfWeek.Sunday)
+        {
+            var start = new DateTime(dt.Year, dt.Month, dt.Day);
+
+            if (start.DayOfWeek != startDayOfWeek)
+            {
+                int d = startDayOfWeek - start.DayOfWeek;
+                if (startDayOfWeek <= start.DayOfWeek)
+                {
+                    return start.AddDays(d);
+                }
+                return start.AddDays(-7 + d);
+            }
+
+            return start;
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that return a DateTime of the first day of the year with the time set to
+        ///     "00:00:00:000". The first moment of the first day of the year.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>A DateTime of the first day of the year with the time set to "00:00:00:000".</returns>
+        public static DateTime StartOfYear(this DateTime @this)
+        {
+            return new DateTime(@this.Year, 1, 1);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that converts the @this to an epoch time span.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>@this as a TimeSpan.</returns>
+        public static TimeSpan ToEpochTimeSpan(this DateTime @this)
+        {
+            return @this.Subtract(new DateTime(1970, 1, 1));
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that tomorrows the given this.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>Tomorrow date at same time.</returns>
+        public static DateTime Tomorrow(this DateTime @this)
+        {
+            return @this.AddDays(1);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that yesterdays the given this.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>Yesterday date at same time.</returns>
+        public static DateTime Yesterday(this DateTime @this)
+        {
+            return @this.AddDays(-1);
+        }
+        #endregion
+
+        #region SetTime
+        /// <summary>
+        ///     Sets the time of the current date with minute precision.
+        /// </summary>
+        /// <param name="current">The current date.</param>
+        /// <param name="hour">The hour.</param>
+        /// <returns>A DateTime.</returns>
+        public static DateTime SetTime(this DateTime current, int hour)
+        {
+            return SetTime(current, hour, 0, 0, 0);
+        }
+
+        /// <summary>
+        ///     Sets the time of the current date with minute precision.
+        /// </summary>
+        /// <param name="current">The current date.</param>
+        /// <param name="hour">The hour.</param>
+        /// <param name="minute">The minute.</param>
+        /// <returns>A DateTime.</returns>
+        public static DateTime SetTime(this DateTime current, int hour, int minute)
+        {
+            return SetTime(current, hour, minute, 0, 0);
+        }
+
+        /// <summary>
+        ///     Sets the time of the current date with second precision.
+        /// </summary>
+        /// <param name="current">The current date.</param>
+        /// <param name="hour">The hour.</param>
+        /// <param name="minute">The minute.</param>
+        /// <param name="second">The second.</param>
+        /// <returns>A DateTime.</returns>
+        public static DateTime SetTime(this DateTime current, int hour, int minute, int second)
+        {
+            return SetTime(current, hour, minute, second, 0);
+        }
+
+        /// <summary>
+        ///     Sets the time of the current date with millisecond precision.
+        /// </summary>
+        /// <param name="current">The current date.</param>
+        /// <param name="hour">The hour.</param>
+        /// <param name="minute">The minute.</param>
+        /// <param name="second">The second.</param>
+        /// <param name="millisecond">The millisecond.</param>
+        /// <returns>A DateTime.</returns>
+        public static DateTime SetTime(this DateTime current, int hour, int minute, int second, int millisecond)
+        {
+            return new DateTime(current.Year, current.Month, current.Day, hour, minute, second, millisecond);
+        }
+        #endregion
+
+        #region 日期类型判断
+        /// <summary>
+        ///     A DateTime extension method that query if '@this' is afternoon.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if afternoon, false if not.</returns>
+        public static bool IsAfternoon(this DateTime @this)
+        {
+            return @this.TimeOfDay >= new DateTime(2000, 1, 1, 12, 0, 0).TimeOfDay;
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if 'date' is date equal.
+        /// </summary>
+        /// <param name="date">The date to act on.</param>
+        /// <param name="dateToCompare">Date/Time of the date to compare.</param>
+        /// <returns>true if date equal, false if not.</returns>
+        public static bool IsDateEqual(this DateTime date, DateTime dateToCompare)
+        {
+            return (date.Date == dateToCompare.Date);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if '@this' is in the future.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if the value is in the future, false if not.</returns>
+        public static bool IsFuture(this DateTime @this)
+        {
+            return @this > DateTime.Now;
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if '@this' is morning.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if morning, false if not.</returns>
+        public static bool IsMorning(this DateTime @this)
+        {
+            return @this.TimeOfDay < new DateTime(2000, 1, 1, 12, 0, 0).TimeOfDay;
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if '@this' is now.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if now, false if not.</returns>
+        public static bool IsNow(this DateTime @this)
+        {
+            return @this == DateTime.Now;
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if '@this' is in the past.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if the value is in the past, false if not.</returns>
+        public static bool IsPast(this DateTime @this)
+        {
+            return @this < DateTime.Now;
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if 'time' is time equal.
+        /// </summary>
+        /// <param name="time">The time to act on.</param>
+        /// <param name="timeToCompare">Date/Time of the time to compare.</param>
+        /// <returns>true if time equal, false if not.</returns>
+        public static bool IsTimeEqual(this DateTime time, DateTime timeToCompare)
+        {
+            return (time.TimeOfDay == timeToCompare.TimeOfDay);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if '@this' is today.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if today, false if not.</returns>
+        public static bool IsToday(this DateTime @this)
+        {
+            return @this.Date == DateTime.Today;
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if '@this' is a week day.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if '@this' is a week day, false if not.</returns>
+        public static bool IsWeekDay(this DateTime @this)
+        {
+            return !(@this.DayOfWeek == DayOfWeek.Saturday || @this.DayOfWeek == DayOfWeek.Sunday);
+        }
+
+        /// <summary>
+        ///     A DateTime extension method that query if '@this' is a weekend day.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if '@this' is a weekend day, false if not.</returns>
+        public static bool IsWeekendDay(this DateTime @this)
+        {
+            return (@this.DayOfWeek == DayOfWeek.Saturday || @this.DayOfWeek == DayOfWeek.Sunday);
+        }
+
+       
+        #endregion
+
+        /// <summary>
+        ///     A T extension method that check if the value is between (exclusif) the minValue and maxValue.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="minValue">The minimum value.</param>
+        /// <param name="maxValue">The maximum value.</param>
+        /// <returns>true if the value is between the minValue and maxValue, otherwise false.</returns>
+        /// ###
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        public static bool Between(this DateTime @this, DateTime minValue, DateTime maxValue)
+        {
+            return minValue.CompareTo(@this) == -1 && @this.CompareTo(maxValue) == -1;
+        }
+
+        /// <summary>
+        ///     A T extension method to determines whether the object is equal to any of the provided values.
+        /// </summary>
+        /// <param name="this">The object to be compared.</param>
+        /// <param name="values">The value list to compare with the object.</param>
+        /// <returns>true if the values list contains the object, else false.</returns>
+        /// ###
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        public static bool In(this DateTime @this, params DateTime[] values)
+        {
+            return Array.IndexOf(values, @this) != -1;
+        }
+
+        /// <summary>
+        ///     A T extension method that check if the value is between inclusively the minValue and maxValue.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="minValue">The minimum value.</param>
+        /// <param name="maxValue">The maximum value.</param>
+        /// <returns>true if the value is between inclusively the minValue and maxValue, otherwise false.</returns>
+        /// ###
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        public static bool InRange(this DateTime @this, DateTime minValue, DateTime maxValue)
+        {
+            return @this.CompareTo(minValue) >= 0 && @this.CompareTo(maxValue) <= 0;
+        }
+
+        /// <summary>
+        ///     A T extension method to determines whether the object is not equal to any of the provided values.
+        /// </summary>
+        /// <param name="this">The object to be compared.</param>
+        /// <param name="values">The value list to compare with the object.</param>
+        /// <returns>true if the values list doesn't contains the object, else false.</returns>
+        /// ###
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        public static bool NotIn(this DateTime @this, params DateTime[] values)
+        {
+            return Array.IndexOf(values, @this) == -1;
+        }
+    }
+}

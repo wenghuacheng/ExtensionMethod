d2d3aaace99861569e13a34611fbf83093919987
wenghuacheng <593103283@qq.com>
2016-04-11 14:15:22 +0800

    1

---
+++ ExtensionMethods/Core/DateTimeOffsetExtensions.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods.Core
+{
+    public static class DateTimeOffsetExtensions
+    {
+        /// <summary>
+        ///     A T extension method that check if the value is between (exclusif) the minValue and maxValue.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="minValue">The minimum value.</param>
+        /// <param name="maxValue">The maximum value.</param>
+        /// <returns>true if the value is between the minValue and maxValue, otherwise false.</returns>
+        /// ###
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        public static bool Between(this DateTimeOffset @this, DateTimeOffset minValue, DateTimeOffset maxValue)
+        {
+            return minValue.CompareTo(@this) == -1 && @this.CompareTo(maxValue) == -1;
+        }
+
+        /// <summary>
+        ///     A T extension method to determines whether the object is equal to any of the provided values.
+        /// </summary>
+        /// <param name="this">The object to be compared.</param>
+        /// <param name="values">The value list to compare with the object.</param>
+        /// <returns>true if the values list contains the object, else false.</returns>
+        /// ###
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        public static bool In(this DateTimeOffset @this, params DateTimeOffset[] values)
+        {
+            return Array.IndexOf(values, @this) != -1;
+        }
+
+        /// <summary>
+        ///     A T extension method that check if the value is between inclusively the minValue and maxValue.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="minValue">The minimum value.</param>
+        /// <param name="maxValue">The maximum value.</param>
+        /// <returns>true if the value is between inclusively the minValue and maxValue, otherwise false.</returns>
+        /// ###
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        public static bool InRange(this DateTimeOffset @this, DateTimeOffset minValue, DateTimeOffset maxValue)
+        {
+            return @this.CompareTo(minValue) >= 0 && @this.CompareTo(maxValue) <= 0;
+        }
+
+        /// <summary>
+        ///     A T extension method to determines whether the object is not equal to any of the provided values.
+        /// </summary>
+        /// <param name="this">The object to be compared.</param>
+        /// <param name="values">The value list to compare with the object.</param>
+        /// <returns>true if the values list doesn't contains the object, else false.</returns>
+        /// ###
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        public static bool NotIn(this DateTimeOffset @this, params DateTimeOffset[] values)
+        {
+            return Array.IndexOf(values, @this) == -1;
+        }
+
+        /// <summary>
+        ///     Converts a time to the time in a particular time zone.
+        /// </summary>
+        /// <param name="dateTimeOffset">The date and time to convert.</param>
+        /// <param name="destinationTimeZone">The time zone to convert  to.</param>
+        /// <returns>The date and time in the destination time zone.</returns>
+        public static DateTimeOffset ConvertTime(this DateTimeOffset dateTimeOffset, TimeZoneInfo destinationTimeZone)
+        {
+            return TimeZoneInfo.ConvertTime(dateTimeOffset, destinationTimeZone);
+        }
+
+        /// <summary>
+        ///     Converts a time to the time in another time zone based on the time zone&#39;s identifier.
+        /// </summary>
+        /// <param name="dateTimeOffset">The date and time to convert.</param>
+        /// <param name="destinationTimeZoneId">The identifier of the destination time zone.</param>
+        /// <returns>The date and time in the destination time zone.</returns>
+        public static DateTimeOffset ConvertTimeBySystemTimeZoneId(this DateTimeOffset dateTimeOffset, String destinationTimeZoneId)
+        {
+            return TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateTimeOffset, destinationTimeZoneId);
+        }
+
+        ///     Sets the time of the current date with minute precision.
+        /// </summary>
+        /// <param name="current">The current date.</param>
+        /// <param name="hour">The hour.</param>
+        /// <returns>A DateTimeOffset.</returns>
+        public static DateTimeOffset SetTime(this DateTimeOffset current, int hour)
+        {
+            return SetTime(current, hour, 0, 0, 0);
+        }
+
+        /// <summary>
+        ///     Sets the time of the current date with minute precision.
+        /// </summary>
+        /// <param name="current">The current date.</param>
+        /// <param name="hour">The hour.</param>
+        /// <param name="minute">The minute.</param>
+        /// <returns>A DateTimeOffset.</returns>
+        public static DateTimeOffset SetTime(this DateTimeOffset current, int hour, int minute)
+        {
+            return SetTime(current, hour, minute, 0, 0);
+        }
+
+        /// <summary>
+        ///     Sets the time of the current date with second precision.
+        /// </summary>
+        /// <param name="current">The current date.</param>
+        /// <param name="hour">The hour.</param>
+        /// <param name="minute">The minute.</param>
+        /// <param name="second">The second.</param>
+        /// <returns>A DateTimeOffset.</returns>
+        public static DateTimeOffset SetTime(this DateTimeOffset current, int hour, int minute, int second)
+        {
+            return SetTime(current, hour, minute, second, 0);
+        }
+
+        /// <summary>
+        ///     Sets the time of the current date with millisecond precision.
+        /// </summary>
+        /// <param name="current">The current date.</param>
+        /// <param name="hour">The hour.</param>
+        /// <param name="minute">The minute.</param>
+        /// <param name="second">The second.</param>
+        /// <param name="millisecond">The millisecond.</param>
+        /// <returns>A DateTimeOffset.</returns>
+        public static DateTimeOffset SetTime(this DateTimeOffset current, int hour, int minute, int second, int millisecond)
+        {
+            return new DateTime(current.Year, current.Month, current.Day, hour, minute, second, millisecond);
+        }
+    }
+}

1c83da8c3676024b21bc8dfbc3a25142d63793c2
wenghuacheng <593103283@qq.com>
2016-04-11 15:06:01 +0800

    1

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExtensionMethods.Core;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime();
+        }
+    }
+}

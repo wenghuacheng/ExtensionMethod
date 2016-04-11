cc3ceac44a276fd25b872a0b787548990eccbf98
wenghuacheng <593103283@qq.com>
2016-04-11 14:57:55 +0800

    1

---
+++ ExtensionMethods/Core/EnumExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods.Core
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        ///     An object extension method that gets description attribute.
+        /// </summary>
+        /// <param name="value">The value to act on.</param>
+        /// <returns>The description attribute.</returns>
+        public static string GetCustomAttributeDescription(this Enum value)
+        {
+            var attr = value.GetType().GetField(value.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault() as DescriptionAttribute;
+            return attr.Description;
+        }
+
+        /// <summary>
+        ///     A T extension method to determines whether the object is equal to any of the provided values.
+        /// </summary>
+        /// <param name="this">The object to be compared.</param>
+        /// <param name="values">The value list to compare with the object.</param>
+        /// <returns>true if the values list contains the object, else false.</returns>
+        public static bool In(this Enum @this, params Enum[] values)
+        {
+            return Array.IndexOf(values, @this) != -1;
+        }
+
+        /// <summary>
+        ///     A T extension method to determines whether the object is not equal to any of the provided values.
+        /// </summary>
+        /// <param name="this">The object to be compared.</param>
+        /// <param name="values">The value list to compare with the object.</param>
+        /// <returns>true if the values list doesn't contains the object, else false.</returns>
+        public static bool NotIn(this Enum @this, params Enum[] values)
+        {
+            return Array.IndexOf(values, @this) == -1;
+        }
+    }
+}
---
+++ ExtensionMethods/Core/GuidExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods.Core
+{
+    public static class GuidExtensions
+    {
+        /// <summary>A GUID extension method that query if '@this' is empty.</summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if empty, false if not.</returns>
+        public static bool IsEmpty(this Guid @this)
+        {
+            return @this == Guid.Empty;
+        }
+
+        /// <summary>A GUID extension method that queries if a not is empty.</summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>true if a not is empty, false if not.</returns>
+        public static bool IsNotEmpty(this Guid @this)
+        {
+            return @this != Guid.Empty;
+        }
+    }
+}

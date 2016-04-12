56fd9cdff0ae2fe4968af90a6b3e5bbf7eb46ef4
wenghuacheng <593103283@qq.com>
2016-04-12 08:33:21 +0800

    注释

--- ExtensionMethods/Core/ArrayExtensions.cs
+++ ExtensionMethods/Core/ArrayExtensions.cs
@@ -180,48 +180,36 @@ namespace ExtensionMethods.Core
         }
 
         /// <summary>
-        ///     Searches for the specified object and returns the index of the last occurrence within the entire one-
-        ///     dimensional .
+        /// 对象匹配集合的最后一个索引位置
         /// </summary>
-        /// <param name="array">The one-dimensional  to search.</param>
-        /// <param name="value">The object to locate in .</param>
-        /// <returns>
-        ///     The index of the last occurrence of  within the entire , if found; otherwise, the lower bound of the array
-        ///     minus 1.
-        /// </returns>
+        /// <param name="array">搜索集合</param>
+        /// <param name="value">搜索对象</param>
+        /// <returns>对象位置</returns>
         public static Int32 LastIndexOf(this Array array, Object value)
         {
             return Array.LastIndexOf(array, value);
         }
 
         /// <summary>
-        ///     Searches for the specified object and returns the index of the last occurrence within the range of elements
-        ///     in the one-dimensional  that extends from the first element to the specified index.
+        /// 对象匹配集合的最后一个索引位置
         /// </summary>
-        /// <param name="array">The one-dimensional  to search.</param>
-        /// <param name="value">The object to locate in .</param>
-        /// <param name="startIndex">The starting index of the backward search.</param>
-        /// <returns>
-        ///     The index of the last occurrence of  within the range of elements in  that extends from the first element to ,
-        ///     if found; otherwise, the lower bound of the array minus 1.
-        /// </returns>
+        /// <param name="array">搜索集合</param>
+        /// <param name="value">搜索对象</param>
+        /// <param name="startIndex">搜索起始位置</param>
+        /// <returns>对象位置</returns>
         public static Int32 LastIndexOf(this Array array, Object value, Int32 startIndex)
         {
             return Array.LastIndexOf(array, value, startIndex);
         }
 
         /// <summary>
-        ///     Searches for the specified object and returns the index of the last occurrence within the range of elements
-        ///     in the one-dimensional  that contains the specified number of elements and ends at the specified index.
+        /// 对象匹配集合的最后一个索引位置
         /// </summary>
-        /// <param name="array">The one-dimensional  to search.</param>
-        /// <param name="value">The object to locate in .</param>
-        /// <param name="startIndex">The starting index of the backward search.</param>
-        /// <param name="count">The number of elements in the section to search.</param>
-        /// <returns>
-        ///     The index of the last occurrence of  within the range of elements in  that contains the number of elements
-        ///     specified in  and ends at , if found; otherwise, the lower bound of the array minus 1.
-        /// </returns>
+        /// <param name="array">搜索集合</param>
+        /// <param name="value">搜索对象</param>
+        /// <param name="startIndex">搜索起始位置</param>
+        /// <param name="count">搜索长度</param>
+        /// <returns>对象位置</returns>
         public static Int32 LastIndexOf(this Array array, Object value, Int32 startIndex, Int32 count)
         {
             return Array.LastIndexOf(array, value, startIndex, count);
@@ -230,20 +218,20 @@ namespace ExtensionMethods.Core
 
         #region Reverse
         /// <summary>
-        ///     Reverses the sequence of the elements in the entire one-dimensional .
+        /// 翻转数组中的顺序
         /// </summary>
-        /// <param name="array">The one-dimensional  to reverse.</param>
+        /// <param name="array">数组</param>
         public static void Reverse(this Array array)
         {
             Array.Reverse(array);
         }
 
         /// <summary>
-        ///     Reverses the sequence of the elements in a range of elements in the one-dimensional .
+        /// 翻转数组中的顺序
         /// </summary>
-        /// <param name="array">The one-dimensional  to reverse.</param>
-        /// <param name="index">The starting index of the section to reverse.</param>
-        /// <param name="length">The number of elements in the section to reverse.</param>
+        /// <param name="array">数组</param>
+        /// <param name="index">起始位置</param>
+        /// <param name="length">长度</param>
         public static void Reverse(this Array array, Int32 index, Int32 length)
         {
             Array.Reverse(array, index, length);
@@ -252,198 +240,102 @@ namespace ExtensionMethods.Core
 
         #region Sort
         /// <summary>
-        ///     Sorts the elements in an entire one-dimensional  using the  implementation of each element of the .
+        /// 通过对象的IComparable接口进行对象排序
         /// </summary>
-        /// <param name="array">The one-dimensional  to sort.</param>
+        /// <param name="array">排序数组</param>
         public static void Sort(this Array array)
         {
             Array.Sort(array);
         }
 
         /// <summary>
-        ///     Sorts a pair of one-dimensional  objects (one contains the keys and the other contains the corresponding
-        ///     items) based on the keys in the first  using the  implementation of each key.
+        /// 通过对象的IComparable接口进行对象排序
         /// </summary>
-        /// <param name="array">The one-dimensional  to sort.</param>
-        /// <param name="items">
-        ///     The one-dimensional  that contains the items that correspond to each of the keys in the .-or-
-        ///     null to sort only the .
-        /// </param>
-        /// ###
-        /// <param name="keys">The one-dimensional  that contains the keys to sort.</param>
-        public static void Sort(this Array array, Array items)
-        {
-            Array.Sort(array, items);
-        }
-
-        /// <summary>
-        ///     Sorts the elements in a range of elements in a one-dimensional  using the  implementation of each element of
-        ///     the .
-        /// </summary>
-        /// <param name="array">The one-dimensional  to sort.</param>
-        /// <param name="index">The starting index of the range to sort.</param>
-        /// <param name="length">The number of elements in the range to sort.</param>
+        /// <param name="array">排序数组</param>
+        /// <param name="index">起始索引</param>
+        /// <param name="length">长度</param>
         public static void Sort(this Array array, Int32 index, Int32 length)
         {
             Array.Sort(array, index, length);
         }
 
         /// <summary>
-        ///     Sorts a range of elements in a pair of one-dimensional  objects (one contains the keys and the other contains
-        ///     the corresponding items) based on the keys in the first  using the  implementation of each key.
+        /// 通过IComparable接口进行对象排序
         /// </summary>
-        /// <param name="array">The one-dimensional  to sort.</param>
-        /// <param name="items">
-        ///     The one-dimensional  that contains the items that correspond to each of the keys in the .-or-
-        ///     null to sort only the .
-        /// </param>
-        /// <param name="index">The starting index of the range to sort.</param>
-        /// <param name="length">The number of elements in the range to sort.</param>
-        /// ###
-        /// <param name="keys">The one-dimensional  that contains the keys to sort.</param>
-        public static void Sort(this Array array, Array items, Int32 index, Int32 length)
-        {
-            Array.Sort(array, items, index, length);
-        }
-
-        /// <summary>
-        ///     Sorts the elements in a one-dimensional  using the specified .
-        /// </summary>
-        /// <param name="array">The one-dimensional  to sort.</param>
-        /// <param name="comparer">
-        ///     The  implementation to use when comparing elements.-or-null to use the  implementation of
-        ///     each element.
-        /// </param>
+        /// <param name="array">排序数组</param>
+        /// <param name="comparer">排序接口</param>
         public static void Sort(this Array array, IComparer comparer)
         {
             Array.Sort(array, comparer);
         }
 
         /// <summary>
-        ///     Sorts a pair of one-dimensional  objects (one contains the keys and the other contains the corresponding
-        ///     items) based on the keys in the first  using the specified .
+        /// 通过IComparable接口进行对象排序
         /// </summary>
-        /// <param name="array">The one-dimensional  to sort.</param>
-        /// <param name="items">
-        ///     The one-dimensional  that contains the items that correspond to each of the keys in the .-or-
-        ///     null to sort only the .
-        /// </param>
-        /// <param name="comparer">
-        ///     The  implementation to use when comparing elements.-or-null to use the  implementation of
-        ///     each element.
-        /// </param>
-        /// ###
-        /// <param name="keys">The one-dimensional  that contains the keys to sort.</param>
-        public static void Sort(this Array array, Array items, IComparer comparer)
-        {
-            Array.Sort(array, items, comparer);
-        }
-
-        /// <summary>
-        ///     Sorts the elements in a range of elements in a one-dimensional  using the specified .
-        /// </summary>
-        /// <param name="array">The one-dimensional  to sort.</param>
-        /// <param name="index">The starting index of the range to sort.</param>
-        /// <param name="length">The number of elements in the range to sort.</param>
-        /// <param name="comparer">
-        ///     The  implementation to use when comparing elements.-or-null to use the  implementation of
-        ///     each element.
-        /// </param>
+        /// <param name="array">排序数组</param>
+        /// <param name="index">起始索引</param>
+        /// <param name="length">长度</param>
+        /// <param name="comparer">排序接口</param>
         public static void Sort(this Array array, Int32 index, Int32 length, IComparer comparer)
         {
             Array.Sort(array, index, length, comparer);
-        }
-
-        /// <summary>
-        ///     Sorts a range of elements in a pair of one-dimensional  objects (one contains the keys and the other contains
-        ///     the corresponding items) based on the keys in the first  using the specified .
-        /// </summary>
-        /// <param name="array">The one-dimensional  to sort.</param>
-        /// <param name="items">
-        ///     The one-dimensional  that contains the items that correspond to each of the keys in the .-or-
-        ///     null to sort only the .
-        /// </param>
-        /// <param name="index">The starting index of the range to sort.</param>
-        /// <param name="length">The number of elements in the range to sort.</param>
-        /// <param name="comparer">
-        ///     The  implementation to use when comparing elements.-or-null to use the  implementation of
-        ///     each element.
-        /// </param>
-        /// ###
-        /// <param name="keys">The one-dimensional  that contains the keys to sort.</param>
-        public static void Sort(this Array array, Array items, Int32 index, Int32 length, IComparer comparer)
-        {
-            Array.Sort(array, items, index, length, comparer);
-        }
+        }       
         #endregion
 
         /// <summary>
-        ///     Copies a specified number of bytes from a source array starting at a particular offset to a destination array
-        ///     starting at a particular offset.
+        ///  将指定数目的字节从起始于特定偏移量的源数组复制到起始于特定偏移量的目标数组
         /// </summary>
-        /// <param name="src">The source buffer.</param>
-        /// <param name="srcOffset">The zero-based byte offset into .</param>
-        /// <param name="dst">The destination buffer.</param>
-        /// <param name="dstOffset">The zero-based byte offset into .</param>
-        /// <param name="count">The number of bytes to copy.</param>
+        /// <param name="src">源数组</param>
+        /// <param name="srcOffset">源数组偏移量</param>
+        /// <param name="dst">目标数组</param>
+        /// <param name="dstOffset">目标数组的字节偏移量</param>
+        /// <param name="count">要复制的字节数</param>
         public static void BlockCopy(this Array src, Int32 srcOffset, Array dst, Int32 dstOffset, Int32 count)
         {
             Buffer.BlockCopy(src, srcOffset, dst, dstOffset, count);
         }
 
         /// <summary>
-        ///     Returns the number of bytes in the specified array.
+        /// 获取数组中字节数
         /// </summary>
-        /// <param name="array">An array.</param>
-        /// <returns>The number of bytes in the array.</returns>
+        /// <param name="array">数组</param>
+        /// <returns>字节数</returns>
         public static Int32 ByteLength(this Array array)
         {
             return Buffer.ByteLength(array);
         }
 
         /// <summary>
-        ///     Retrieves the byte at a specified location in a specified array.
+        /// 获取指定位置的字节数
         /// </summary>
-        /// <param name="array">An array.</param>
-        /// <param name="index">A location in the array.</param>
-        /// <returns>Returns the  byte in the array.</returns>
+        /// <param name="array">数组</param>
+        /// <param name="index">指定位置</param>
+        /// <returns>字节</returns>
         public static Byte GetByte(this Array array, Int32 index)
         {
             return Buffer.GetByte(array, index);
         }
 
         /// <summary>
-        ///     Assigns a specified value to a byte at a particular location in a specified array.
+        /// 将指定的值分配给指定数组中特定位置处的字节。
         /// </summary>
-        /// <param name="array">An array.</param>
-        /// <param name="index">A location in the array.</param>
-        /// <param name="value">A value to assign.</param>
+        /// <param name="array">数组</param>
+        /// <param name="index">指定位置</param>
+        /// <param name="value">指定值</param>
         public static void SetByte(this Array array, Int32 index, Byte value)
         {
             Buffer.SetByte(array, index, value);
         }
 
         /// <summary>
-        ///     An Array extension method that check if the array is lower then the specified index.
+        /// 索引是否超过意味数组界限
         /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <param name="index">Zero-based index of the.</param>
+        /// <param name="this">数组</param>
+        /// <param name="index">判断的索引</param>
         /// <returns>true if it succeeds, false if it fails.</returns>
         public static bool WithinIndex(this Array @this, int index)
         {
             return index >= 0 && index < @this.Length;
-        }
-
-        /// <summary>
-        ///     An Array extension method that check if the array is lower then the specified index.
-        /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <param name="index">Zero-based index of the.</param>
-        /// <param name="dimension">(Optional) the dimension.</param>
-        /// <returns>true if it succeeds, false if it fails.</returns>
-        public static bool WithinIndex(this Array @this, int index, int dimension = 0)
-        {
-            return index >= @this.GetLowerBound(dimension) && index <= @this.GetUpperBound(dimension);
         }
     }
 }

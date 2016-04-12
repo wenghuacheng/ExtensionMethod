f230534f9cc52ea921bbff674bb0997f71787a39
wenghuacheng <593103283@qq.com>
2016-04-12 15:13:36 +0800

    注释

---
+++ ExtensionMethods/Serialization/DeserializeExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+
+namespace ExtensionMethods.Serialization
+{
+    public static class DeserializeExtensions
+    {
+        #region BinaryFormatter
+        /// <summary>
+        /// 二进制反序列化
+        /// </summary>
+        public static T DeserializeBinary<T>(this string @this)
+        {
+            using (var stream = new MemoryStream(Encoding.Default.GetBytes(@this)))
+            {
+                var binaryRead = new BinaryFormatter();
+                return (T)binaryRead.Deserialize(stream);
+            }
+        }
+
+        /// <summary>
+        /// 二进制反序列化
+        /// </summary>
+        public static T DeserializeBinary<T>(this string @this, Encoding encoding)
+        {
+            using (var stream = new MemoryStream(encoding.GetBytes(@this)))
+            {
+                var binaryRead = new BinaryFormatter();
+                return (T)binaryRead.Deserialize(stream);
+            }
+        }
+        #endregion
+
+        #region JavaScriptSerializer
+        /// <summary>
+        /// 异步通信层内部使用
+        /// </summary>
+        public static T DeserializeJavaScript<T>(this string @this)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<T>(@this);
+        }
+        #endregion
+
+        #region DataContractJsonSerializer
+        /// <summary>
+        /// Json反序列化
+        /// </summary>
+        public static T DeserializeJson<T>(this string @this)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+
+            using (var stream = new MemoryStream(Encoding.Default.GetBytes(@this)))
+            {
+                return (T)serializer.ReadObject(stream);
+            }
+        }
+
+        /// <summary>
+        /// Json反序列化
+        /// </summary>
+        public static T DeserializeJson<T>(this string @this, Encoding encoding)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+
+            using (var stream = new MemoryStream(encoding.GetBytes(@this)))
+            {
+                return (T)serializer.ReadObject(stream);
+            }
+        }
+        #endregion
+
+        #region XmlSerializer
+        /// <summary>
+        /// Xml反序列化
+        /// </summary>
+        public static T DeserializeXml<T>(this string @this)
+        {
+            var x = new XmlSerializer(typeof(T));
+            var r = new StringReader(@this);
+
+            return (T)x.Deserialize(r);
+        }
+        #endregion
+    }
+}
---
+++ ExtensionMethods/Serialization/SerializeExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+
+namespace ExtensionMethods.Serialization
+{
+    public static class SerializeExtensions
+    {
+        #region BinaryFormatter
+        /// <summary>
+        /// 二进制序列化
+        /// </summary>
+        public static string SerializeBinary<T>(this T @this)
+        {
+            var binaryWrite = new BinaryFormatter();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                binaryWrite.Serialize(memoryStream, @this);
+                return Encoding.Default.GetString(memoryStream.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// 二进制序列化
+        /// </summary>
+        public static string SerializeBinary<T>(this T @this, Encoding encoding)
+        {
+            var binaryWrite = new BinaryFormatter();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                binaryWrite.Serialize(memoryStream, @this);
+                return encoding.GetString(memoryStream.ToArray());
+            }
+        }
+        #endregion
+
+        #region JavaScriptSerializer
+        /// <summary>
+        /// 异步通信层内部使用
+        /// </summary>
+        public static string SerializeJavaScript<T>(this T @this)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(@this);
+        }
+        #endregion
+
+        #region DataContractJsonSerializer
+        /// <summary>
+        /// Json序列化
+        /// </summary>
+        public static string SerializeJson<T>(this T @this)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                serializer.WriteObject(memoryStream, @this);
+                return Encoding.Default.GetString(memoryStream.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Json序列化
+        /// </summary>
+        public static string SerializeJson<T>(this T @this, Encoding encoding)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                serializer.WriteObject(memoryStream, @this);
+                return encoding.GetString(memoryStream.ToArray());
+            }
+        }
+        #endregion
+
+        #region XmlSerializer
+        /// <summary>
+        /// XML序列化
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>The string representation of the Xml Serialization.</returns>
+        public static string SerializeXml(this object @this)
+        {
+            var xmlSerializer = new XmlSerializer(@this.GetType());
+
+            using (var stringWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(stringWriter, @this);
+                using (var streamReader = new StringReader(stringWriter.GetStringBuilder().ToString()))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+        #endregion
+    }
+}

bd94243deea00ac6e6912de3112bf723ff5c382c
wenghuacheng <593103283@qq.com>
2016-04-12 12:43:35 +0800

    注释

--- ExtensionMethods/Core/GuidExtensions.cs
+++ ExtensionMethods/Core/GuidExtensions.cs
@@ -7,17 +7,13 @@ namespace ExtensionMethods.Core
 {
     public static class GuidExtensions
     {
-        /// <summary>A GUID extension method that query if '@this' is empty.</summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>true if empty, false if not.</returns>
+        /// <summary>GUID是否为空</summary>
         public static bool IsEmpty(this Guid @this)
         {
             return @this == Guid.Empty;
         }
 
-        /// <summary>A GUID extension method that queries if a not is empty.</summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>true if a not is empty, false if not.</returns>
+        /// <summary>GUID是否不为空</summary>
         public static bool IsNotEmpty(this Guid @this)
         {
             return @this != Guid.Empty;
--- ExtensionMethods/Core/Int32Extensions.cs
+++ ExtensionMethods/Core/Int32Extensions.cs
@@ -8,73 +8,34 @@ namespace ExtensionMethods.Core
 {
     public static class Int32Extensions
     {
+        #region 类型判断
         /// <summary>
-        ///     An Int32 extension method that days the given this.
+        /// 是否是偶数
         /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>A TimeSpan.</returns>
-        public static TimeSpan Days(this Int32 @this)
-        {
-            return TimeSpan.FromDays(@this);
-        }
-
-        /// <summary>
-        ///     An Int32 extension method that factor of.
-        /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <param name="factorNumer">The factor numer.</param>
-        /// <returns>true if it succeeds, false if it fails.</returns>
-        public static bool FactorOf(this Int32 @this, Int32 factorNumer)
-        {
-            return factorNumer % @this == 0;
-        }
-
-        /// <summary>
-        ///     An Int32 extension method that hours the given this.
-        /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>A TimeSpan.</returns>
-        public static TimeSpan Hours(this Int32 @this)
-        {
-            return TimeSpan.FromHours(@this);
-        }
-
-        /// <summary>
-        ///     An Int32 extension method that query if '@this' is even.
-        /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>true if even, false if not.</returns>
         public static bool IsEven(this Int32 @this)
         {
             return @this % 2 == 0;
         }
 
         /// <summary>
-        ///     An Int32 extension method that query if '@this' is multiple of.
+        /// 是否是factor的倍数
         /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <param name="factor">The factor.</param>
-        /// <returns>true if multiple of, false if not.</returns>
         public static bool IsMultipleOf(this Int32 @this, Int32 factor)
         {
             return @this % factor == 0;
         }
 
         /// <summary>
-        ///     An Int32 extension method that query if '@this' is odd.
+        /// 是否是奇数
         /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>true if odd, false if not.</returns>
         public static bool IsOdd(this Int32 @this)
         {
             return @this % 2 != 0;
         }
 
         /// <summary>
-        ///     An Int32 extension method that query if '@this' is prime.
+        /// 是否是素数
         /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>true if prime, false if not.</returns>
         public static bool IsPrime(this Int32 @this)
         {
             if (@this == 1 || @this == 2)
@@ -100,17 +61,43 @@ namespace ExtensionMethods.Core
         }
 
         /// <summary>
-        ///     An Int32 extension method that milliseconds the given this.
+        /// 是否是闰年
         /// </summary>
-        /// <param name="this">The @this to act on.</param>
-        /// <returns>A TimeSpan.</returns>
+        /// <param name="year">A 4-digit year.</param>
+        /// <returns>true if  is a leap year; otherwise, false.</returns>
+        public static Boolean IsLeapYear(this Int32 year)
+        {
+            return DateTime.IsLeapYear(year);
+        }
+        #endregion
+
+        #region TiemSpan
+        /// <summary>
+        /// 获取天为单位时间间隔
+        /// </summary>
+        public static TimeSpan Days(this Int32 @this)
+        {
+            return TimeSpan.FromDays(@this);
+        }
+
+        /// <summary>
+        /// 获取小时为单位时间间隔
+        /// </summary>
+        public static TimeSpan Hours(this Int32 @this)
+        {
+            return TimeSpan.FromHours(@this);
+        }
+
+        /// <summary>
+        /// 获取毫秒为单位时间间隔
+        /// </summary>
         public static TimeSpan Milliseconds(this Int32 @this)
         {
             return TimeSpan.FromMilliseconds(@this);
         }
 
         /// <summary>
-        ///     An Int32 extension method that minutes the given this.
+        /// 获取分为单位时间间隔
         /// </summary>
         /// <param name="this">The @this to act on.</param>
         /// <returns>A TimeSpan.</returns>
@@ -120,7 +107,7 @@ namespace ExtensionMethods.Core
         }
 
         /// <summary>
-        ///     An Int32 extension method that seconds the given this.
+        /// 获取秒为单位时间间隔
         /// </summary>
         /// <param name="this">The @this to act on.</param>
         /// <returns>A TimeSpan.</returns>
@@ -130,7 +117,7 @@ namespace ExtensionMethods.Core
         }
 
         /// <summary>
-        ///     An Int32 extension method that weeks the given this.
+        /// 获取周为单位时间间隔
         /// </summary>
         /// <param name="this">The @this to act on.</param>
         /// <returns>A TimeSpan.</returns>
@@ -138,140 +125,74 @@ namespace ExtensionMethods.Core
         {
             return TimeSpan.FromDays(@this * 7);
         }
+        #endregion
 
-        /// <summary>
-        ///     Returns the specified 32-bit signed integer value as an array of bytes.
         /// </summary>
-        /// <param name="value">The number to convert.</param>
-        /// <returns>An array of bytes with length 4.</returns>
-        public static Byte[] GetBytes(this Int32 value)
-        {
-            return BitConverter.GetBytes(value);
-        }
-
-        /// <summary>
-        ///     Converts the specified Unicode code point into a UTF-16 encoded string.
+        /// 指定年月中的天数
         /// </summary>
-        /// <param name="utf32">A 21-bit Unicode code point.</param>
-        /// <returns>
-        ///     A string consisting of one  object or a surrogate pair of  objects equivalent to the code point specified by
-        ///     the  parameter.
-        /// </returns>
-        public static String ConvertFromUtf32(this Int32 utf32)
-        {
-            return Char.ConvertFromUtf32(utf32);
-        }
-
-        /// <summary>
-        ///     Returns the number of days in the specified month and year.
-        /// </summary>
-        /// <param name="year">The year.</param>
-        /// <param name="month">The month (a number ranging from 1 to 12).</param>
-        /// <returns>
-        ///     The number of days in  for the specified .For example, if  equals 2 for February, the return value is 28 or
-        ///     29 depending upon whether  is a leap year.
-        /// </returns>
         public static Int32 DaysInMonth(this Int32 year, Int32 month)
         {
             return DateTime.DaysInMonth(year, month);
         }
 
-        /// <summary>
-        ///     Returns an indication whether the specified year is a leap year.
-        /// </summary>
-        /// <param name="year">A 4-digit year.</param>
-        /// <returns>true if  is a leap year; otherwise, false.</returns>
-        public static Boolean IsLeapYear(this Int32 year)
-        {
-            return DateTime.IsLeapYear(year);
-        }
-
         #region Math
         /// <summary>
-        ///     Returns the absolute value of a 32-bit signed integer.
+        /// 绝对值
         /// </summary>
-        /// <param name="value">A number that is greater than , but less than or equal to .</param>
-        /// <returns>A 32-bit signed integer, x, such that 0 ? x ?.</returns>
         public static Int32 Abs(this Int32 value)
         {
             return Math.Abs(value);
         }
 
         /// <summary>
-        ///     Produces the full product of two 32-bit numbers.
+        /// 两个 32 位数字的完整乘积
         /// </summary>
-        /// <param name="a">The first number to multiply.</param>
-        /// <param name="b">The second number to multiply.</param>
-        /// <returns>The number containing the product of the specified numbers.</returns>
         public static Int64 BigMul(this Int32 a, Int32 b)
         {
             return Math.BigMul(a, b);
         }
 
         /// <summary>
-        ///     An Int32 extension method that div rem.
+        /// 计算两个 32 位有符号整数的商，并通过输出参数返回余数
         /// </summary>
-        /// <param name="a">a to act on.</param>
-        /// <param name="b">The Int32 to process.</param>
-        /// <param name="result">[out] The result.</param>
-        /// <returns>An Int32.</returns>
+        /// <param name="a">被除数</param>
+        /// <param name="b">除数</param>
+        /// <param name="result">余数</param>
+        /// <returns>指定数字的商</returns>
         public static Int32 DivRem(this Int32 a, Int32 b, out Int32 result)
         {
             return Math.DivRem(a, b, out result);
         }
 
         /// <summary>
-        ///     Returns the larger of two 32-bit signed integers.
+        /// 最大值
         /// </summary>
-        /// <param name="val1">The first of two 32-bit signed integers to compare.</param>
-        /// <param name="val2">The second of two 32-bit signed integers to compare.</param>
-        /// <returns>Parameter  or , whichever is larger.</returns>
         public static Int32 Max(this Int32 val1, Int32 val2)
         {
             return Math.Max(val1, val2);
         }
 
         /// <summary>
-        ///     Returns the smaller of two 32-bit signed integers.
+        /// 最小值
         /// </summary>
-        /// <param name="val1">The first of two 32-bit signed integers to compare.</param>
-        /// <param name="val2">The second of two 32-bit signed integers to compare.</param>
-        /// <returns>Parameter  or , whichever is smaller.</returns>
         public static Int32 Min(this Int32 val1, Int32 val2)
         {
             return Math.Min(val1, val2);
         }
 
-        /// <summary>
-        ///     Returns a value indicating the sign of a 32-bit signed integer.
-        /// </summary>
-        /// <param name="value">A signed number.</param>
-        /// <returns>
-        ///     A number that indicates the sign of , as shown in the following table.Return value Meaning -1  is less than
-        ///     zero. 0  is equal to zero. 1  is greater than zero.
-        /// </returns>
-        public static Int32 Sign(this Int32 value)
-        {
-            return Math.Sign(value);
-        }
-
         #endregion
 
         /// <summary>
-        ///     Converts an integer value from host byte order to network byte order.
+        /// 将整数值由主机字节顺序转换为网络字节顺序
         /// </summary>
-        /// <param name="host">The number to convert, expressed in host byte order.</param>
-        /// <returns>An integer value, expressed in network byte order.</returns>
         public static Int32 HostToNetworkOrder(this Int32 host)
         {
             return IPAddress.HostToNetworkOrder(host);
         }
 
         /// <summary>
-        ///     Converts an integer value from network byte order to host byte order.
+        /// 将整数值由网络字节顺序转换为主机字节顺序
         /// </summary>
-        /// <param name="network">The number to convert, expressed in network byte order.</param>
-        /// <returns>An integer value, expressed in host byte order.</returns>
         public static Int32 NetworkToHostOrder(this Int32 network)
         {
             return IPAddress.NetworkToHostOrder(network);

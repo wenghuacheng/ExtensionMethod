e963d62e4ae0885420ecd3a5205c37e832872870
wenghuacheng <593103283@qq.com>
2016-04-12 14:01:28 +0800

    注释

---
+++ ExtensionMethods/IO/StreamExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ExtensionMethods.IO
+{
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// 读取所有内容
+        /// </summary>
+        public static string ReadToEnd(this Stream @this)
+        {
+            using (var sr = new StreamReader(@this, Encoding.Default))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// 读取所有内容
+        /// </summary>
+        public static string ReadToEnd(this Stream @this, Encoding encoding)
+        {
+            using (var sr = new StreamReader(@this, encoding))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// 读取所有内容
+        /// </summary>
+        public static string ReadToEnd(this Stream @this, long position)
+        {
+            @this.Position = position;
+
+            using (var sr = new StreamReader(@this, Encoding.Default))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// 读取所有内容
+        /// </summary>
+        public static string ReadToEnd(this Stream @this, Encoding encoding, long position)
+        {
+            @this.Position = position;
+
+            using (var sr = new StreamReader(@this, encoding))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// 获取字节数组
+        /// </summary>
+        public static byte[] ToByteArray(this Stream @this)
+        {
+            using (var ms = new MemoryStream())
+            {
+                @this.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// 获取MD5加密字符串
+        /// </summary>
+        public static string ToMD5Hash(this Stream @this)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hashBytes = md5.ComputeHash(@this);
+                var sb = new StringBuilder();
+                foreach (byte bytes in hashBytes)
+                {
+                    sb.Append(bytes.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
